054b86f454788585606dc64eed916ac6b70e8690
Starling73 <63596872+Starling73@users.noreply.github.com>
2021-05-31 11:25:53 +0300

    add lab 2a

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MND_L2a
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            Children.Add(new Part2());
+        }
+    }
+}
